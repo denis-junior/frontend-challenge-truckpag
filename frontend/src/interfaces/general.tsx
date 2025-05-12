362f80de99439a8b389a5728bfd95581a6251e7b
denis junior <denis.junior@azimutyachts.com.br>
2025-05-12 00:45:51 -0300

    feat: arquivo para interfaces mais relevantes como Body API

---
+++ frontend/src/interfaces/general.tsx
@@ -0,0 +1,34 @@
+export interface IOptionSelect {
+  value: string;
+  label: string;
+}
+
+export interface Ili {
+  label: string;
+  icon?: React.ReactNode;
+  active: boolean;
+  onClick: () => void;
+  backColor?: string;
+}
+
+export interface IMovie {
+  id: string;
+  title: string;
+  original_title: string;
+  original_title_romanised: string;
+  image: string;
+  movie_banner: string;
+  description: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  running_time: string;
+  rt_score: string;
+  people: string[];
+  species: string[];
+  locations: string[];
+  vehicles: string[];
+  url: string;
+  rating?: number;
+  notes?: string;
+}
